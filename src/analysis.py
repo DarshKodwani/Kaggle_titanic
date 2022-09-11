e007128a8321620508d9cf975431ce1b63e270ab
Darsh Kodwani <ddkcdk@yahoo.co.uk>
2022-09-11 19:43:07 +0100

    playing and visualizing with data

--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,9 @@
-import numpy as np
+import os
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+inputs_folder = os.path.join(os.environ["REPO_ROOT"], "input_data")
+train_raw = pd.read_csv(os.path.join(inputs_folder, "train.csv"))
+test_raw = pd.read_csv(os.path.join(inputs_folder, "test.csv"))
+profile = ProfileReport(train_raw, title="Training Data Report")
+profile.to_file("Training_data_report.html")
