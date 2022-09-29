1f5a21c1a2aea69266e11c0e300c86625195c558
Darsh Kodwani <ddkcdk@yahoo.co.uk>
2022-09-29 17:24:06 +0100

    adding NN regressor model

--- src/analysis.py
+++ src/analysis.py
@@ -1,9 +1,27 @@
 import os
 import pandas as pd
-from pandas_profiling import ProfileReport
+import tensorflow as tf
+import numpy as np
 
 inputs_folder = os.path.join(os.environ["REPO_ROOT"], "input_data")
 train_raw = pd.read_csv(os.path.join(inputs_folder, "train.csv"))
 test_raw = pd.read_csv(os.path.join(inputs_folder, "test.csv"))
-profile = ProfileReport(train_raw, title="Training Data Report")
-profile.to_file("Training_data_report.html")
+features = ['SibSp', 'Pclass', 'Sex', 'Parch']
+
+y_train = train_raw['Survived']
+X_train = train_raw[features]
+X_test = test_raw[features]
+
+X_train_one_hot = pd.get_dummies(X_train)
+X_test_one_hot = pd.get_dummies(X_test)
+
+model = tf.keras.Sequential([
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1, activation = 'sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='Adam', metrics='accuracy')
+model.fit(x=X_train_one_hot, y=y_train, epochs=200)
+
+y_train_preds = np.round(model.predict(X_train_one_hot)).squeeze()
+y_test_preds = np.round(model.predict(X_test_one_hot)).squeeze()
